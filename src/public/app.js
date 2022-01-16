78185537f033ca7a25d5fd104af859359c690bbe
mustapha3341 <mustapha3341@gmail.com>
2022-01-16 11:30:41 +0100

    updated all deps

--- src/public/app.js
+++ src/public/app.js
@@ -1,23 +1,28 @@
 const myForm = document.getElementById("myForm");
 const inputField = myForm.querySelector("input");
 
-const getWeatherData = (event) => {
+const getWeatherData = async (event) => {
   event.preventDefault();
   const city = inputField.value;
   // perform api request to the server to get
   // weather data
-  fetch("/", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ city })
-  })
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch((error) => {
-      console.log(error);
-    });
+
+  try {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ city }),
+    };
+
+    const response = await fetch("/", options);
+    const weather_data = await response.json();
+
+    console.log(weather_data);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 myForm.addEventListener("submit", getWeatherData);
